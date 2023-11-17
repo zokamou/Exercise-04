3edb90fb41663e3e877dcd0b3eca769575f2c261
zfeller <zfeller@ucsc.edu>
2023-11-17 15:54:02 -0800

    start

--- src/Overworld.js
+++ src/Overworld.js
@@ -9,17 +9,59 @@ class Overworld extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         })
+        this.load.image('tilesetImage', 'tileset.png')
+        this.load.tilemapTiledJSON('tilemapJSON', 'overworld.json')
     }
 
     create() {
         // velocity constant
         this.VEL = 100
 
+        //tilemap info
+        const map = this.add.tilemap('tilemapJSON')
+
+        //USE THE NAME OF THE TILE IN TILED
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage')
+
+        //layers
+        const bgLayer = map.createLayer('Grass', tileset, 0,0)
+        const terrainLayer = map.createLayer('Terrain', tileset, 0,0)
+    
+
         // add slime
-        this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
+        const slimeSpawn = map.findObject('Spawns', obj => obj.name === 'slime_spawn')
+        this.slime = this.physics.add.sprite(slimeSpawn.x, slimeSpawn.y, 'slime', 0)
         this.slime.body.setCollideWorldBounds(true)
 
+        //trees
+        const treesLayer = map.createLayer('Trees', tileset, 0,0)
+
         // slime animation
+        this.anims.create({
+            key: 'jiggle', 
+            frameRate: 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime', {
+                start: 0,
+                end: 1
+            })
+        })
+
+        this.slime.play('jiggle')
+
+        // camera
+        this.cameras.main.setBounds(0,0,map.widthInPixels, map.heightInPixels)
+        this.cameras.main.startFollow(this.slime, true, 0.25, 0.25)
+        this.physics.world.setBounds(0,0,map.widthInPixels, map.heightInPixels)
+        terrainLayer.setCollisionByProperty({
+            collides: true
+        })
+        treesLayer.setCollisionByProperty({
+            collides: true
+        })
+
+        this.physics.add.collider(this.slime, terrainLayer)
+        this.physics.add.collider(this.slime, treesLayer)
 
         // input
         this.cursors = this.input.keyboard.createCursorKeys()
